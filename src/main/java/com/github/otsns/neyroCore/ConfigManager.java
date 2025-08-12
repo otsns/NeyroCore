6a6ada537f8a50c35c137e53ebe75da28d7094bf
otsns <34196965+otsns@users.noreply.github.com>
2025-08-12 12:39:43 +0300

    Update ConfigManager.java

--- src/main/java/com/github/otsns/neyroCore/ConfigManager.java
+++ src/main/java/com/github/otsns/neyroCore/ConfigManager.java
@@ -7,8 +7,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.nio.charset.StandardCharsets;
-
 public class ConfigManager {
     private final JavaPlugin plugin;
     private boolean enabled;
@@ -31,14 +29,13 @@ public class ConfigManager {
 
     public void updateOnlinePlayersBrand() {
         if (!enabled || !Bukkit.getPluginManager().isPluginEnabled("ProtocolLib")) return;
-    
+        
         for (Player player : Bukkit.getOnlinePlayers()) {
             try {
-            // Новый метод для 1.21+
                 PacketContainer brandPacket = new PacketContainer(PacketType.Play.Server.SERVER_DATA);
                 brandPacket.getStrings().write(0, serverBrand);
                 brandPacket.getBooleans().write(0, false); // previewsChat = false
-            
+                
                 ProtocolLibrary.getProtocolManager().sendServerPacket(player, brandPacket);
             } catch (Exception e) {
                 plugin.getLogger().warning("Error updating brand for " + player.getName() + ": " + e.getMessage());
