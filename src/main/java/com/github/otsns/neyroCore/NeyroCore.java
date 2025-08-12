67231b2b9c7bea216d1c385c944df7317f487e1b
otsns <34196965+otsns@users.noreply.github.com>
2025-08-12 09:45:50 +0300

    Update NeyroCore.java

--- src/main/java/com/github/otsns/neyroCore/NeyroCore.java
+++ src/main/java/com/github/otsns/neyroCore/NeyroCore.java
@@ -1,26 +1,45 @@
-package com.github.otsns.neyrocore;
+package com.github.otsns.neyroCore;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerSendBrandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NeyroCorePlugin extends JavaPlugin {
-    private BrandManager brandManager;
+public class NeyroCore extends JavaPlugin implements Listener {
+    private boolean enabled;
+    private String serverBrand;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        this.brandManager = new BrandManager(this);
-        
-        getServer().getPluginManager().registerEvents(
-            new PacketListener(this, brandManager), 
-            this
-        );
-        
-        getLogger().info("NeyroCore activated! Custom brand: " + 
-            getConfig().getString("brand-name"));
+        reloadConfigValues();
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("NeyroCore enabled!");
+    }
+
+    private void reloadConfigValues() {
+        reloadConfig();
+        enabled = getConfig().getBoolean("enabled", true);
+        serverBrand = getConfig().getString("server-brand", "NeyroCore");
+    }
+
+    @EventHandler
+    public void onBrandSend(AsyncPlayerSendBrandEvent event) {
+        if (enabled) {
+            event.setBrand(serverBrand);
+        }
     }
 
     @Override
-    public void onDisable() {
-        getLogger().info("NeyroCore disabled");
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("core") && args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+            reloadConfigValues();
+            sender.sendMessage(ChatColor.GREEN + "NeyroCore config reloaded!");
+            return true;
+        }
+        return false;
     }
 }
