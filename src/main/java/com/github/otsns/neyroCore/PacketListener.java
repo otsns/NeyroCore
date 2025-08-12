9e6fcda2f2349a2687b6962d16d0e4ab16348da7
otsns <34196965+otsns@users.noreply.github.com>
2025-08-12 10:46:13 +0300

    Create PacketListener.java

---
+++ src/main/java/com/github/otsns/neyroCore/PacketListener.java
@@ -0,0 +1,43 @@
+package com.github.otsns.neyroCore;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import org.bukkit.plugin.Plugin;
+
+public class PacketListener {
+    private final Plugin plugin;
+    private final ConfigManager configManager;
+    private final PacketAdapter adapter;
+
+    public PacketListener(Plugin plugin, ConfigManager configManager) {
+        this.plugin = plugin;
+        this.configManager = configManager;
+        this.adapter = createAdapter();
+    }
+
+    private PacketAdapter createAdapter() {
+        return new PacketAdapter(plugin, PacketType.Play.Server.LOGIN) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                if (!configManager.isEnabled()) return;
+
+                PacketContainer packet = event.getPacket();
+                // Изменяем server brand
+                packet.getChatComponents().write(0, WrappedChatComponent.fromText(configManager.getServerBrand()));
+            }
+        };
+    }
+
+    public void register() {
+        ProtocolLibrary.getProtocolManager().addPacketListener(adapter);
+    }
+
+    public void unregister() {
+        ProtocolLibrary.getProtocolManager().removePacketListener(adapter);
+    }
+}
