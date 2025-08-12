f73060ceb8b3c6e9249433eb0b80200944cc93d0
otsns <34196965+otsns@users.noreply.github.com>
2025-08-12 13:28:36 +0300

    Update NeyroCore.java

--- src/main/java/com/github/otsns/neyroCore/NeyroCore.java
+++ src/main/java/com/github/otsns/neyroCore/NeyroCore.java
@@ -1,101 +1,68 @@
 package com.github.otsns.neyroCore;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
 import java.util.logging.Level;
 
 public class NeyroCore extends JavaPlugin {
 
     private ConfigManager configManager;
-    private ProtocolManager protocolManager;
-    private PacketListener packetListener;
+    private BrandListener brandListener;
 
     @Override
     public void onEnable() {
-        // Проверка наличия ProtocolLib
-        if (!Bukkit.getPluginManager().isPluginEnabled("ProtocolLib")) {
-            getLogger().severe("ProtocolLib is required but not found! Disabling plugin...");
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
+        saveDefaultConfig();
+        this.configManager = new ConfigManager(this);
+        this.configManager.reload();
 
-        // Инициализация конфигурации
-        configManager = new ConfigManager(this);
-        configManager.loadConfig();
+        this.brandListener = new BrandListener(this, configManager);
+        // register listener
+        getServer().getPluginManager().registerEvents(brandListener, this);
 
-        try {
-            // Инициализация ProtocolLib
-            protocolManager = ProtocolLibrary.getProtocolManager();
-            packetListener = new PacketListener(this, configManager);
-            packetListener.register();
-            getLogger().info("Packet listener registered successfully");
-        } catch (Exception e) {
-            getLogger().log(Level.SEVERE, "Failed to register packet listener", e);
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
+        // register plugin channels for safety (incoming/outgoing)
+        // older versions use "MC|Brand", newer use "minecraft:brand"
+        Bukkit.getMessenger().registerIncomingPluginChannel(this, "minecraft:brand", brandListener);
+        Bukkit.getMessenger().registerIncomingPluginChannel(this, "MC|Brand", brandListener);
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "minecraft:brand");
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "MC|Brand");
 
-        // Регистрация команды
-        registerCommand();
-        
-        getLogger().info("NeyroCore v" + getDescription().getVersion() + " enabled!");
-        getLogger().info("Server brand set to: " + configManager.getServerBrand());
-    }
-
-    private void registerCommand() {
-        try {
-            // Регистрация команды
-            Objects.requireNonNull(this.getCommand("core")).setExecutor((sender, command, label, args) -> {
-                if (args.length == 0) {
-                    sender.sendMessage("§eNeyroCore v" + getDescription().getVersion());
-                    sender.sendMessage("§eUsage: /core reload");
-                    return true;
-                }
-
-                if (args[0].equalsIgnoreCase("reload")) {
-                    if (!sender.hasPermission("neyrocore.reload")) {
-                        sender.sendMessage("§cYou don't have permission to reload the plugin!");
-                        return true;
-                    }
-
-                    try {
-                        configManager.reloadConfig();
-                        sender.sendMessage("§aConfiguration reloaded successfully!");
-                        
-                        // Обновляем бренд для всех онлайн-игроков
-                        configManager.updateOnlinePlayersBrand();
-                        sender.sendMessage("§aServer brand updated for all online players!");
-                        return true;
-                    } catch (Exception e) {
-                        sender.sendMessage("§cError reloading configuration: " + e.getMessage());
-                        getLogger().log(Level.SEVERE, "Error reloading configuration", e);
-                        return true;
-                    }
-                }
-
-                sender.sendMessage("§eUsage: /core reload");
-                return true;
-            });
-            getLogger().info("Command '/core' registered successfully");
-        } catch (Exception e) {
-            getLogger().log(Level.SEVERE, "Critical error registering command", e);
-        }
+        getLogger().info("NeyroCore enabled. Brand: " + ChatColor.stripColor(configManager.getServerBrand()));
     }
 
     @Override
     public void onDisable() {
-        if (packetListener != null) {
-            packetListener.unregister();
-            getLogger().info("Packet listener unregistered");
-        }
+        Bukkit.getMessenger().unregisterIncomingPluginChannel(this, "minecraft:brand", brandListener);
+        Bukkit.getMessenger().unregisterIncomingPluginChannel(this, "MC|Brand", brandListener);
+        Bukkit.getMessenger().unregisterOutgoingPluginChannel(this, "minecraft:brand");
+        Bukkit.getMessenger().unregisterOutgoingPluginChannel(this, "MC|Brand");
         getLogger().info("NeyroCore disabled");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("core")) {
+            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+                if (!sender.hasPermission("neyrocore.reload")) {
+                    sender.sendMessage(ChatColor.RED + "You don't have permission!");
+                    return true;
+                }
+                try {
+                    configManager.reload();
+                    sender.sendMessage(ChatColor.GREEN + "NeyroCore reloaded");
+                    getLogger().info("Configuration reloaded via command by " + sender.getName());
+                } catch (Exception e) {
+                    getLogger().log(Level.WARNING, "Failed to reload config", e);
+                    sender.sendMessage(ChatColor.RED + "Reload failed: " + e.getMessage());
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
     public ConfigManager getConfigManager() {
