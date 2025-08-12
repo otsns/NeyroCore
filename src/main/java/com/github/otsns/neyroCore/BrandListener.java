ddeec27aa997819510c5cee0a7f2c2e7a419dc9e
otsns <34196965+otsns@users.noreply.github.com>
2025-08-12 13:32:47 +0300

    Update BrandListener.java

--- src/main/java/com/github/otsns/neyroCore/BrandListener.java
+++ src/main/java/com/github/otsns/neyroCore/BrandListener.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.server.PluginMessageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.nio.charset.StandardCharsets;
